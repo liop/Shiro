d5d19b6ec6da2fde361f0e616f364125dc0daa9f
liop <just.liop@qq.com>
2025-01-24 11:31:28 +0800

    add anayltics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import '../styles/index.css'
 
 import type { PropsWithChildren } from 'react'
+import { Analytics } from "@vercel/analytics/react"
 
 import { init } from './init'
 import { InitInClient } from './InitInClient'
@@ -8,10 +9,10 @@ import { InitInClient } from './InitInClient'
 init()
 export default async function RootLayout({ children }: PropsWithChildren) {
   return (
-    <>
+    <Analytics>
       {children}
       <InitInClient />
       {/* <BrowserSupport /> */}
-    </>
+    </Analytics>
   )
 }
